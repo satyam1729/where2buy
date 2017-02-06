7b511804b9bfa34a38b4971380b198611115c542
Satyam Yadav <satyamyadav13579@gmail.com>
2017-02-06 18:31:03 +0530

    add flipkart search

---
+++ readimg/flipkart.py
@@ -0,0 +1,13 @@
+#u can add price comparision for lower price search
+
+import json
+import requests
+headers = {'Fk-Affiliate-Id':'satyam66h','Fk-Affiliate-Token':'1454d0d14f774904a1c9bc2a7b924923'}
+
+def flipkartsearch(text):
+	url='https://affiliate-api.flipkart.net/affiliate/1.0/search.json?query='+text+'&resultCount=1'
+	r = requests.get(url, headers=headers).json()
+	title=r['productInfoList'][0]['productBaseInfoV1']['title']
+	price=r['productInfoList'][0]['productBaseInfoV1']['flipkartSpecialPrice']['amount']
+	return title,price
+#print flipkartsearch('sony+mobile')[0]
